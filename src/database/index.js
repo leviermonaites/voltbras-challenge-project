7a39dd46a3947fc9425d351b22a419aab4485e6d
Levi Ermonaites De Freitas <68472819+leviermonaites@users.noreply.github.com>
2021-06-22 18:02:31 -0300

    Increasing database connection delay

--- src/database/index.js
+++ src/database/index.js
@@ -11,43 +11,43 @@ const initialize = async () => {
   const { host, port, username: user, password, database } = databaseConfig;
 
   try {
-    setTimeout(() => { // I'm using setTimeout to handle the first the mysql server turns on. I was trying to solve it with docker-compose-wait but didn't manage to do it successfully.
-    const connection = await mysql.createConnection({
-      host,
-      port,
-      user,
-      password,
-    });
+    setTimeout(() => {
+      // I'm using setTimeout to handle the first the mysql server turns on. I was trying to solve it with docker-compose-wait but didn't manage to do it successfully.
+      const connection = await mysql.createConnection({
+        host,
+        port,
+        user,
+        password,
+      });
 
+      await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+      // connect to db
+      const sequelize = new Sequelize(databaseConfig);
 
-    // connect to db
-    const sequelize = new Sequelize(databaseConfig);
+      // Init Models
+      initStation(sequelize);
+      initUser(sequelize);
+      initRecharge(sequelize);
 
-    // Init Models
-    initStation(sequelize);
-    initUser(sequelize);
-    initRecharge(sequelize);
+      // Define relationships.
+      User.hasMany(Recharge, {
+        foreignKey: "user_id",
+      });
+      Recharge.belongsTo(User, {
+        foreignKey: "user_id",
+      });
 
-    // Define relationships.
-    User.hasMany(Recharge, {
-      foreignKey: "user_id",
-    });
-    Recharge.belongsTo(User, {
-      foreignKey: "user_id",
-    });
+      Station.hasMany(Recharge, {
+        foreignKey: "station_id",
+      });
+      Recharge.belongsTo(Station, {
+        foreignKey: "station_id",
+      });
 
-    Station.hasMany(Recharge, {
-      foreignKey: "station_id",
-    });
-    Recharge.belongsTo(Station, {
-      foreignKey: "station_id",
-    });
-
-    // sync all models with database
-    await sequelize.sync();
-  }, 2000);
+      // sync all models with database
+      await sequelize.sync();
+    }, 5000);
   } catch (e) {
     console.log(e);
   }
